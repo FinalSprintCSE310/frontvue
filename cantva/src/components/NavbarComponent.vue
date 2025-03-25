323186a30f7e9d73c52fcf3ba76b52b4edcae4d5
MichaelL0827 <m1ch03l@byui.edu>
2025-03-24 19:35:06 -0600

    login page added

--- cantva/src/components/NavbarComponent.vue
+++ cantva/src/components/NavbarComponent.vue
@@ -1,11 +1,12 @@
-import { RouterLink } from 'vue-router';
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
     <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
     </nav>
 </template>
 <style scoped>
